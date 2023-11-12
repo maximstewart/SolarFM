2de4de6b22ebb80fc49d87200b58bd7d4fde7fbb
itdominator <1itdominator@gmail.com>
2023-11-12 00:49:16 -0600

    Restructuring src folder

---
+++ debs/chown_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+function main() {
+    sudo chown -R "${USER}":"${USER}" .
+}
+main;
--- src/solarfm/core/mixins/signals_mixins.py
+++ src/solarfm/core/mixins/signals_mixins.py
@@ -1,11 +1,11 @@
 # Python imports
 
 # Lib imports
+
+# Application imports
 from .signals.file_action_signals_mixin import FileActionSignalsMixin
 from .signals.ipc_signals_mixin import IPCSignalsMixin
 from .signals.keyboard_signals_mixin import KeyboardSignalsMixin
-
-# Application imports
 
 
 
