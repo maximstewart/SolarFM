a47bd23e78aebb97ec3e5a2f078bd1147c0a878e
itdominator <1itdominator@gmail.com>
2024-02-08 21:24:01 -0600

    made main method

--- src/solarfm/__main__.py
+++ src/solarfm/__main__.py
@@ -3,9 +3,11 @@
 # Python imports
 import argparse
 import faulthandler
-import locale
 import traceback
 from setproctitle import setproctitle
+
+import tracemalloc
+tracemalloc.start()
 
 # Lib imports
 
@@ -15,38 +17,38 @@ from app import Application
 
 
 
-def run():
-    try:
-        locale.setlocale(locale.LC_NUMERIC, 'C')
+def main(args, unknownargs):
+    setproctitle(f'{app_name}')
 
-        setproctitle(f"{app_name}")
-        # faulthandler.enable()  # For better debug info
+    if args.debug == "true":
+        settings_manager.set_debug(True)
 
-        parser = argparse.ArgumentParser()
-        # Add long and short arguments
-        parser.add_argument("--debug", "-d", default="false", help="Do extra console messaging.")
-        parser.add_argument("--trace-debug", "-td", default="false", help="Disable saves, ignore IPC lock, do extra console messaging.")
-        parser.add_argument("--no-plugins", "-np", default="false", help="Do not load plugins.")
+    if args.trace_debug == "true":
+        settings_manager.set_trace_debug(True)
 
-        parser.add_argument("--new-tab", "-t", default="", help="Open a file into new tab.")
-        parser.add_argument("--new-window", "-w", default="", help="Open a file into a new window.")
+    settings_manager.do_dirty_start_check()
+    Application(args, unknownargs)
 
-        # Read arguments (If any...)
-        args, unknownargs = parser.parse_known_args()
-
-        if args.debug == "true":
-            settings_manager.set_debug(True)
-
-        if args.trace_debug == "true":
-            settings_manager.set_trace_debug(True)
-
-        settings_manager.do_dirty_start_check()
-        Application(args, unknownargs)
-    except Exception as e:
-        traceback.print_exc()
-        quit()
 
 
 if __name__ == "__main__":
-    """ Set process title, get arguments, and create GTK main thread. """
-    run()
+    ''' Set process title, get arguments, and create GTK main thread. '''
+
+    parser = argparse.ArgumentParser()
+    # Add long and short arguments
+    parser.add_argument("--debug", "-d", default="false", help="Do extra console messaging.")
+    parser.add_argument("--trace-debug", "-td", default="false", help="Disable saves, ignore IPC lock, do extra console messaging.")
+    parser.add_argument("--no-plugins", "-np", default="false", help="Do not load plugins.")
+
+    parser.add_argument("--new-tab", "-nt", default="false", help="Opens a 'New Tab' if a handler is set for it.")
+    parser.add_argument("--file", "-f", default="default", help="JUST SOME FILE ARG.")
+
+    # Read arguments (If any...)
+    args, unknownargs = parser.parse_known_args()
+
+    try:
+        faulthandler.enable()  # For better debug info
+        main(args, unknownargs)
+    except Exception as e:
+        traceback.print_exc()
+        quit()
